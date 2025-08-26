4e118d9547635ce7bb44bf92cb081630540d9370
MADHAV KALRA <68096582+madhavkalra7@users.noreply.github.com>
2025-08-26 18:14:54 +0530

    Add files via upload

---
+++ src/app/projects/[projectId]/page.tsx
@@ -0,0 +1,35 @@
+import { getQueryClient , trpc} from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ProjectView } from "@/modules/projects/ui/views/project-view";
+import { ErrorBoundary } from "react-error-boundary"
+
+interface Props {
+    params: Promise<{
+        projectId: string;
+    }>;
+}
+
+const Page = async ({ params }: Props) => {
+    const { projectId } = await params;
+
+    const queryClient= getQueryClient();
+    void queryClient.prefetchQuery(trpc.messages.getMany.queryOptions({
+        projectId,
+    }));
+    void queryClient.prefetchQuery(trpc.projects.getOne.queryOptions({
+        id: projectId,
+    }));
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <ErrorBoundary fallback={<p>Error!</p>}>
+                <Suspense fallback={<p>Loading...</p>}>
+                    <ProjectView projectId={projectId} />
+                </Suspense>
+            </ErrorBoundary>
+            {/* Project id : {projectId} */}
+        </HydrationBoundary>
+    );
+};
+
+export default Page;
